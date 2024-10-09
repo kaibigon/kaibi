f74984cce3ab25dc0605c6ebca02a30978e30c70
kaibigon <kaiwang980527@icloud.com>
2024-10-09 21:14:36 +0200

    restruct

--- kaibi/core/Logger.h
+++ kaibi/core/Logger.h
@@ -24,3 +24,17 @@ namespace KAIBI
     };
 
 } // namespace kaibi
+
+// Core log macros
+#define KAIBI_CORE_TRACE(...)   ::KAIBI::Logger::GetCoreLogger()->trace(__VA_ARGS__)
+#define KAIBI_CORE_INFO(...)    ::KAIBI::Logger::GetCoreLogger()->info(__VA_ARGS__)
+#define KAIBI_CORE_WARN(...)    ::KAIBI::Logger::GetCoreLogger()->warn(__VA_ARGS__)
+#define KAIBI_CORE_ERROR(...)   ::KAIBI::Logger::GetCoreLogger()->error(__VA_ARGS__)
+#define KAIBI_CORE_CRITICAL(...) ::KAIBI::Logger::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define KAIBI_CLIENT_TRACE(...) ::KAIBI::Logger::GetClientLogger()->trace(__VA_ARGS__)
+#define KAIBI_CLIENT_INFO(...)  ::KAIBI::Logger::GetClientLogger()->info(__VA_ARGS__)
+#define KAIBI_CLIENT_WARN(...)  ::KAIBI::Logger::GetClientLogger()->warn(__VA_ARGS__)
+#define KAIBI_CLIENT_ERROR(...) ::KAIBI::Logger::GetClientLogger()->error(__VA_ARGS__)
+#define KAIBI_CLIENT_CRITICAL(...) ::KAIBI::Logger::GetClientLogger()->critical(__VA_ARGS__)
