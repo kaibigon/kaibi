c4043605146915f0bd69b0b04bffc23d295241eb
CORP\kai.wang <kaiwang980527@icloud.com>
2026-05-20 14:17:30 +0200

    remove old project, add d3d12 dependencies

--- README.md
+++
@@ -1,3 +0,0 @@
-### install xmake for building the project
-
-### Roadmap
--- xmake.lua
+++ xmake.lua
@@ -1,47 +1,27 @@
-set_project("nmsl")
-add_rules("mode.debug", "mode.release")
-add_requires("spdlog", "glfw", "glad", "glm")
-add_requires("imgui docking", {config = {glfw_opengl3 = true}})
+set_project("RenderTest")
+set_version("0.1.0")
+set_xmakever("2.8.0")
 
 set_languages("c++20")
+set_warnings("all", "error")
 
-if is_os("windows") then
-    add_defines("KB_PLATFORM_WINDOWS")
-elseif is_os("linux") then
-    add_defines("KB_PLATFORM_LINUX")
-elseif is_os("macosx") then
-    add_defines("KB_PLATFORM_MACOS")
-end
+add_rules("mode.debug", "mode.release")
 
-function traverse_directory(path)
-    add_headerfiles(path .. "/**.h")
-    add_includedirs(path)
-    for _, dir in ipairs(os.dirs(path .. "/*")) do
-        add_headerfiles(dir .. "/**.h")
-        add_includedirs(dir)
-    end
-end
-
-target("KAIBI")
-
-    set_kind("shared")
-    add_defines("KB_BUILD_DLL")
-    add_defines("KB_GRAPHICS_OPENGL")
-    add_files("runtime/**.cpp")
-    -- add_headerfiles("kaibi/**.h")
-    add_includedirs("runtime/")
-    set_pcxxheader("runtime/pch.h")
-
-
-    add_packages("spdlog", {public = true})
-    add_packages("glfw", {public = true})
-    add_packages("imgui", {public = true})
-    add_packages("glad", {public = true})
-    add_packages("glm", {public = true})
-    -- set_pcxxheader("kaibi/src/kaibi.h")
-
-target("TESTBED")
+target("RenderTest")
     set_kind("binary")
-    add_files("testbed/src/**.cpp")
-    add_deps("KAIBI")
-    traverse_directory("runtime")
+    add_files("src/*.cpp")
+
+    if is_plat("windows") then
+        add_defines("UNICODE", "_UNICODE", "NOMINMAX", "WIN32_LEAN_AND_MEAN")
+        add_ldflags("/SUBSYSTEM:WINDOWS", {force = true})
+        add_syslinks(
+            "d3d12",
+            "dxgi",
+            "d3dcompiler",
+            "dxguid",
+            "user32",
+            "gdi32",
+            "kernel32",
+            "ole32"
+        )
+    end
