7470a189a5205e3fc3788bb1bfc6e7a979af68a8
CORP\kai.wang <kaiwang980527@icloud.com>
2026-05-26 16:11:17 +0200

    ignore xmake file

--- build/.gens/rules/plugin.compile_commands.autoupdate.d
+++ build/.gens/rules/plugin.compile_commands.autoupdate.d
@@ -1,10 +1,10 @@
 {
-    files = {
-        [[C:\Users\KaiWang\Desktop\kaibi\xmake.lua]],
-        [[C:\Users\KaiWang\Desktop\kaibi\.xmake\windows\x64\xmake.conf]]
-    },
     values = {
         [[src\DXDebugLayer.cpp]],
         [[src\main.cpp]]
+    },
+    files = {
+        [[C:\Users\KaiWang\Desktop\kaibi\xmake.lua]],
+        [[C:\Users\KaiWang\Desktop\kaibi\.xmake\windows\x64\xmake.conf]]
     }
 }
